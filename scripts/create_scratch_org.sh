02ab9ebc76eef91b121eb2dcda2fc1802300df66
therealskv <therealskv79@gmail.com>
2024-11-19 00:23:21 -0500

    Initial commit

---
+++ scripts/create_scratch_org.sh
@@ -0,0 +1,8 @@
+sf org create scratch \
+    --target-dev-hub sathishv \
+    --definition-file config/project-scratch-def.json \
+    --alias sf-code-examples-1118 \
+    --duration-days 30 \
+    --username sathishv.sfcodeexamples@sathishv.com.1118 \
+    --admin-email sathish.velayudam@gmail.com \
+    --json
